c89ff14428aa02e013e9bf23fbabe4bf769dcf13
Victor <victorgomezdejuan@gmail.com>
2023-05-02 17:46:01 +0200

    Sum fractions with negative addends

--- AddFractionsTests/AddFractionTests.cs
+++ AddFractionsTests/AddFractionTests.cs
@@ -46,5 +46,6 @@ public class AddFractionTests {
         Assert.Equal(new Fraction(3, 8), new Fraction(2, 4).Add(new Fraction(-1, 8)));
         Assert.Equal(new Fraction(5, 12), new Fraction(3, 4).Add(new Fraction(1, -3)));
         Assert.Equal(new Fraction(-1, 12), new Fraction(1, 4).Add(new Fraction(-1, 3)));
+        Assert.Equal(new Fraction(-7, 6), new Fraction(-1, 2).Add(new Fraction(-2, 3)));
     }
 }
