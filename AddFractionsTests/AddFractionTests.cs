b6215a76c4fc1b5a1f99dde69e76e6420da35765
Victor <victorgomezdejuan@gmail.com>
2023-05-02 16:37:53 +0200

    Sum negative whole numbers

--- AddFractionsTests/AddFractionTests.cs
+++ AddFractionsTests/AddFractionTests.cs
@@ -4,13 +4,20 @@ namespace AddFractionsTests;
 
 public class AddFractionTests {
     [Fact]
-    public void OneSummandIsZero() {
+    public void SumPositiveWholeNumberAndZero() {
         Assert.Equal(new Fraction(1), new Fraction(1).Add(new Fraction(0)));
         Assert.Equal(new Fraction(5), new Fraction(0).Add(new Fraction(5)));
     }
 
     [Fact]
-    public void SumWholeNumbers() {
+    public void SumPositiveWholeNumbers() {
         Assert.Equal(new Fraction(2), new Fraction(1).Add(new Fraction(1)));
+    }
+
+    [Fact]
+    public void SumNegativeWholeNumbers() {
+        Assert.Equal(new Fraction(0), new Fraction(1).Add(new Fraction(-1)));
+        Assert.Equal(new Fraction(1), new Fraction(2).Add(new Fraction(-1)));
+        Assert.Equal(new Fraction(-3), new Fraction(-1).Add(new Fraction(-2)));
     }
 }
