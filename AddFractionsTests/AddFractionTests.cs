8073926f67069182334013aa6ae321f6b7cdf81d
gomezdev <victor.gomez_de_juan@contractors.roche.com>
2023-05-02 16:20:26 +0200

    Sum whole numbers

--- AddFractionsTests/AddFractionTests.cs
+++ AddFractionsTests/AddFractionTests.cs
@@ -8,4 +8,9 @@ public class AddFractionTests {
         Assert.Equal(new Fraction(1), new Fraction(1).Add(new Fraction(0)));
         Assert.Equal(new Fraction(5), new Fraction(0).Add(new Fraction(5)));
     }
+
+    [Fact]
+    public void SumWholeNumbers() {
+        Assert.Equal(new Fraction(2), new Fraction(1).Add(new Fraction(1)));
+    }
 }
